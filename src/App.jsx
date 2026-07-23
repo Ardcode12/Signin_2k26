9067d526ffc092e08976f7d2fbe6bc38b8852988
Ardcode12 <ardcode12@gmail.com>
2026-07-23 18:35:10 +0530

    side lack

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ export default function App() {
           position: 'fixed',
           top: 0,
           left: 0,
-          width: '100vw',
+          width: '100%',
           height: '100vh',
           objectFit: 'cover',
           zIndex: -1,
